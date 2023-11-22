b5351deb2f430d202d72990f8f2685ed546f1fc8
AlexandreGazagnes <a_syoez@yahoo.fr>
2023-11-22 14:15:11 +0100

    fix rss

--- src/extracts.py
+++ src/extracts.py
@@ -3,6 +3,34 @@ import requests
 from bs4 import BeautifulSoup
 
 
+def _alternative_rss_url(
+    url,
+    pattern={"type": "application/rss+xml"},
+    verbose=1,
+):
+    """ """
+
+    url = url.split("/")
+    url[-1] = "@" + url[-1]
+    new_url = "/".join(url)
+
+    response = requests.get(new_url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    links = soup.find_all("link", pattern)
+
+    if len(links) == 0:
+        return ""
+
+    if verbose >= 2:
+        logging.error("Alternative RSS URL")
+        logging.error(f"base url {url}")
+        logging.error(f"alternative url {new_url}")
+        logging.error(f"links {links}")
+
+    return links
+
+
 def extract_rss_url(
     url, pattern={"type": "application/rss+xml"}, sleeper=0.2, verbose=1
 ):
@@ -13,7 +41,14 @@ def extract_rss_url(
 
     links = soup.find_all("link", pattern)
 
-    if len(links) == 0:
+    if not links:
+        links = _alternative_rss_url(
+            url=url,
+            pattern=pattern,
+            verbose=verbose,
+        )
+
+    if not links:
         return ""
 
     link = links[0].get("href")
@@ -22,7 +57,7 @@ def extract_rss_url(
     if sleeper > 0:
         time.sleep(sleeper)
 
-    if verbose:
+    if verbose >= 1:
         logging.warning(link)
 
     return link
