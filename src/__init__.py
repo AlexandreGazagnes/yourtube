469254075bc51cf2cc74863d64776e52ab230c00
AlexandreGazagnes <a_syoez@yahoo.fr>
2023-11-28 18:25:16 +0100

    fix init

--- src/__init__.py
+++ src/__init__.py
@@ -1,18 +1,37 @@
 from fastapi import FastAPI
-from src.routers import Router
+
+# from src.routers import Router
 import logging
+
+from src.categ_1.routers import categ_1
+from src.categ_2.routers import categ_2
+from src.channels.routers import channels
+from src.home.routers import home
+from src.languages.routers import languages
+from src.status.routers import status
+
+# from src.users.routers import users
+# from src.userschannels.routers import userschannels
+from src.videos.routers import videos
 
 
 def create_app():
     """Create a FastAPI app instance"""
 
+    # app
     app = FastAPI()
-    app.include_router(Router.home)
-    app.include_router(Router.languages)
-    app.include_router(Router.categ_1)
-    app.include_router(Router.videos)
-    app.include_router(Router.channels)
-    app.include_router(Router.status)
+
+    # routers
+    app.include_router(categ_1)
+    app.include_router(categ_2)
+    app.include_router(channels)
+    app.include_router(home)
+    app.include_router(languages)
+
+    app.include_router(status)
+    # app.include_router(users)
+    # app.include_router(userschannels)
+    app.include_router(videos)
 
     return app
 
