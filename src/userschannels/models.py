d7099f9a543000e4cc02a917c7192f53c1184601
AlexandreGazagnes <a_syoez@yahoo.fr>
2023-11-28 14:52:42 +0100

    add userchannels

--- src/userschannels/models.py
+++ src/userschannels/models.py
@@ -1,13 +1,12 @@
-from typing import List
-from typing import Optional
+import logging
+from typing import List, Optional
 
 from sqlalchemy import create_engine, URL, select
 from sqlalchemy import ForeignKey, String, Column, Integer, Float, DateTime, Date
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, Session
-from src.helpers import make_now, make_token
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-
-from src.models.base import Base
+from src.helpers.helpers import make_now, make_token
+from src.base.models import Base
 
 
 class UsersChannels(Base):
@@ -34,4 +33,3 @@ class UsersChannels(Base):
 
     def __repr__(self) -> str:
         return f"UsersChannels(id_userschannels={self.id_userschannels}, id_user={self.id_user}, id_channel={self.id_channel})"
-
