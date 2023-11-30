bcedf07ad84d3704240eeb2e86c09308f9073582
AlexandreGazagnes <a_syoez@yahoo.fr>
2023-11-30 12:33:42 +0100

    add user preferences get

---
+++ src/users/queries.py
@@ -0,0 +1,60 @@
+import logging
+
+# from src.videos.models.db import Session, engine
+from src.videos.models import Video
+
+from src.helpers.helpers import make_time_delta
+from src.db import Session, engine
+
+from src.channels.models import Channel
+from src.userschannels.models import UserChannel
+from sqlalchemy.sql import text
+
+import pandas as pd
+
+
+def _query_user_counts():
+    pass
+
+
+def _query_all_id_users():
+    pass
+
+
+def _query_preferences(
+    id_user: str,
+):
+    query_string = f""" 
+    select 
+                v.category,
+                u.id_user,
+                cc.id_categ_1,
+                cc.id_categ_2 
+
+    from videos v
+    left join userschannels u on u.id_channel = v.id_channel
+    left join channels c on c.id_channel = v.id_channel
+    left join categ_1 cc on cc.id_categ_1 = c.id_categ_1
+    where u.id_user = {id_user};
+    """
+
+    sql_query = text(query_string)
+
+    with Session(engine) as session:
+        result = session.execute(sql_query)
+
+    keys = result.keys()
+
+    result = [dict(zip(keys, row)) for row in result]
+
+    df = pd.DataFrame(result)
+
+    json = {k: df[k].unique().tolist() for k in df.columns}
+
+    return json
+
+
+class UserQuery:
+    counts = _query_user_counts
+    all_id_users = _query_all_id_users
+    preferences = _query_preferences
---
+++ src/users/routers.py
@@ -0,0 +1,40 @@
+import logging, time, random
+
+from fastapi import FastAPI, HTTPException, APIRouter, Depends
+
+# from src.db import Channel, Video, session, engine, Session
+from src.helpers.routers import jsonify, validate_token  # token_required
+from src.helpers.queries import query_all
+
+from src.users.queries import UserQuery
+
+# from src.queries import query_all, jsonify
+# from src.validators import ChannelBase, default_channel
+# import pandas as pd
+
+# from src.videos.models import Video
+# logging.basicConfig(level=logging.INFO)
+
+users = APIRouter(
+    prefix="",
+    tags=["users"],
+)
+
+
+@users.get("/counts", status_code=200)
+async def users_counts():
+    """counts users in"""
+
+    payload = UserQuery.counts()
+    return jsonify(payload, message="done")
+
+
+@users.get("/{id_user}/preferences", status_code=200)
+async def users_preferences(
+    id_user: str,
+):
+    """get user preferences from db and return as json"""
+
+    payload = UserQuery.preferences(id_user=id_user)
+
+    return jsonify(payload, message="done")
