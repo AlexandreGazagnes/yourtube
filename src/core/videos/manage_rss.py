a9b76f2894b1593c65870486736d0f497d6735c4
AlexandreGazagnes <a_syoez@yahoo.fr>
2023-12-05 15:40:56 +0100

    work on videos

---
+++ src/core/videos/manage_rss.py
@@ -0,0 +1,143 @@
+import logging
+
+from pandarallel import pandarallel
+
+pandarallel.initialize()
+
+import pandas as pd
+import feedparser
+from src.core.paths import RSS
+from src.core.videos.scrap_details import extract_video_detail
+
+
+def _clean_video_dict(video_dict, id_channel=""):
+    """clean a video dict from a rss feed"""
+
+    accepted_keys = [
+        # "link",
+        "yt_videoid",
+        "title",
+        "author",
+        "published",
+        "media_starrating",
+        "media_statistics",
+        # "media_content",
+        # "summary",
+        # "summary_detail",
+        # "media_community",
+    ]
+
+    video_dict = {i: j for i, j in video_dict.items() if i in accepted_keys}
+
+    # id_video
+    video_dict["id_video"] = video_dict["yt_videoid"]
+
+    # media_starrating
+    media_starrating = video_dict.get("media_starrating", {})
+    video_dict["votes"] = int(media_starrating.get("count", -1))
+    video_dict["stars"] = float(media_starrating.get("average", -1.0))
+    # video_dict["stars"] = int(video_dict["stars"])
+
+    # media_statistics
+    video_dict["views"] = int(video_dict.get("media_statistics", {}).get("views", 0))
+
+    # clean dict
+    video_dict = {
+        k: v
+        for k, v in video_dict.items()
+        if k not in ["media_starrating", "media_statistics"]
+    }
+
+    # id_channel
+    video_dict["id_channel"] = id_channel
+
+    # clean title
+    video_dict["title"] = video_dict["title"][:100]
+
+    # TODO:
+    # add thumbnail video url
+    # add category
+    # add keywords
+    # add duration
+
+    return {
+        k: v
+        for k, v in video_dict.items()
+        if k
+        not in [
+            "yt_videoid",
+        ]
+    }
+
+
+# def clean_entries(entries, id_channel=""):
+#     """clean a list of entries from a rss feed"""
+
+#     entries = [_clean_video_dict(i, id_channel) for i in entries]
+
+#     return entries
+
+
+def _extract_rss(id_channel):
+    """extract the rss feed from a channel id"""
+
+    if not id_channel:
+        return []
+
+    # feeds
+    feeds = feedparser.parse(RSS + id_channel)
+
+    # entries
+    entries = feeds.entries
+
+    entries_cleaned = [_clean_video_dict(i, id_channel) for i in entries]
+    # entries_cleaned = clean_entries(entries, id_channel=id_channel)
+
+    return entries_cleaned
+
+
+# def _extract_rss_and_flatten(channel_list_ids):
+#     """ """
+
+#     feeds = [_extract_rss(i) for i in channel_list_ids]
+
+#     new_videos = []
+#     _ = [new_videos.extend(i) for i in feeds]
+
+#     return new_videos
+
+
+def build_rss(
+    df: pd.DataFrame,
+    extract_video_detail: bool = False,
+    verbose: int = 1,
+) -> pd.DataFrame:
+    """build a dataframe from a list of channel ids"""
+
+    # fill blanks if needed
+
+    df.id_channel.fillna("", inplace=True)
+
+    # list of list of feeds
+    clean_entries = df.id_channel.parallel_apply(_extract_rss)
+
+    # flatten the list of list of feeds
+    li = []
+    _ = [li.extend(i) for i in clean_entries]
+
+    # build the dataframe
+    df = pd.DataFrame(li)
+
+    # published to datetime
+    df["published"] = pd.to_datetime(df.published)
+
+    # rename yt_videoid to id_video
+    df.rename(columns={"yt_videoid": "id_video"}, inplace=True)
+
+    if extract_video_detail:
+        details = df.parallel_apply(extract_video_detail, axis=1)
+        df = pd.concat([df, details], axis=1, ignore_index=True)
+
+    logging.info(f"rss feed built : {df}")
+
+    return df
---
+++ src/core/videos/scrap_details.py
@@ -0,0 +1,91 @@
+import logging
+
+import requests
+
+from bs4 import BeautifulSoup
+
+
+def extract_video_detail(
+    video_url: str | None,
+    sleeper=0.1,
+    verbose=1,
+):
+    """extract the video detail from the video url"""
+
+    # update video url if neeeded
+    if not "youtube" in video_url:
+        video_url = f"https://www.youtube.com/watch?v={video_url}"
+
+    # response
+    try:
+        response = requests.get(video_url)
+        if not response.ok:
+            logging.error(f"requests - {response.status_code} - {video_url}")
+            return {}
+    except Exception as e:
+        logging.error(f"requests - {e} - {video_url}")
+        return {}
+
+    # soup
+    try:
+        soup = BeautifulSoup(response.text, "html.parser")
+    except Exception as e:
+        logging.error(f"BeautifulSoup - {e} - {video_url}")
+        return {}
+
+    # duration
+    duration = ""
+    try:
+        pattern = '"lengthSeconds": "'.replace(" ", "")
+        _duration = response.text.split(pattern)[1][:100]
+        _duration = int(_duration.split('"')[0].strip())
+        duration = _duration
+    except Exception as e:
+        logging.error(f"duration - {e} - {video_url} - duration set to -{_duration}")
+
+    # video_thumbnail_url
+    video_thumbnail_url = ""
+    try:
+        video_thumbnail_url = "none"
+        pattern = '"thumbnail": { "thumbnails": [{ "url": '.replace(" ", "")
+        _video_thumbnail_url = response.text.split(pattern)[1][:300]
+        _video_thumbnail_url = _video_thumbnail_url[1:].split('"')[0]
+        video_thumbnail_url = _video_thumbnail_url
+    except Exception as e:
+        logging.error(
+            f"video_thumbnail_url - {e} - {video_url} - video_thumbnail_url set to -{_video_thumbnail_url}"
+        )
+
+    # # channel Thumbnail
+    # pattern = '"channelThumbnail": { "thumbnails": [{ "url": '.replace(" ", "")
+    # channel_thumbnail_url = response.text.split(pattern)[1][:300]
+    # channel_thumbnail_url = channel_thumbnail_url[1:].split('"')[0]
+
+    # keywords
+    keywords = ""
+    try:
+        _keywords = soup.find("meta", {"name": "keywords"}).get("content")[:100]
+        keywords = _keywords
+    except Exception as e:
+        logging.error(f"keywords - {e} - {video_url} - keywords set to -{_keywords}")
+
+    # category
+    category = ""
+    try:
+        pattern = '"category": "'.replace(" ", "")
+        _category = response.text.split(pattern)[1][:100]
+        _category = _category.split('"')[0].strip()
+        category = _category
+    except Exception as e:
+        logging.error(f"category - {e} - {video_url} - category set to -{_category}")
+
+    data = {
+        "thumbnail_video_url": video_thumbnail_url,
+        "category": category,
+        "keywords": keywords[:100],
+        "duration": duration,
+    }
+
+    data = {k: v for k, v in data.items() if v}
+
+    return data
