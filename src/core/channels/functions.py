65d6b25c2480387cfb9175e461dd41928fd9c400
AlexandreGazagnes <a_syoez@yahoo.fr>
2023-12-05 19:41:04 +0100

    core channel implement

---
+++ src/core/channels/functions.py
@@ -0,0 +1,10 @@
+def fix_channels():
+    """Fixes channels to be a list of strings"""
+
+    # load all channels from the database
+
+    # find broken channels
+
+    # perform fix
+
+    # update database in consequence
