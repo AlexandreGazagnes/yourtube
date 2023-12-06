a3af2193717349e8d84941cbdcdb1d54b3c03c64
AlexandreGazagnes <a_syoez@yahoo.fr>
2023-12-06 12:31:18 +0100

    update tests

--- tests/unit/core/videos/test_rss.py
+++ tests/unit/core/videos/test_rss.py
@@ -3,7 +3,7 @@ import pytest
 import pandas as pd
 import logging
 
-from src.core.videos.rss import build_rss, _extract_rss
+from src.core.videos.rss import _scrap_one_rss, _scrap_rss_list, _update_rss_list
 
 from pandarallel import pandarallel
 
@@ -11,11 +11,11 @@ from pandarallel import pandarallel
 class TestRSS:
     """ """
 
-    def test_extract_rss(self):
+    def test_scrap_one_rss(self):
         """ """
 
         id_channel = "UCFqV9b9ji8yIMw3GyNlueuA"
-        results = _extract_rss(id_channel)
+        results = _scrap_one_rss(id_channel)
 
         logging.info(results)
 
@@ -23,27 +23,83 @@ class TestRSS:
         assert results
         assert isinstance(results[0], dict)
 
-    def test_build_rss(self):
+    def test_scrap_rss_list(self):
         """ " """
 
-        df = pd.DataFrame(
-            {
-                "id_channel": [
-                    "UCFqV9b9ji8yIMw3GyNlueuA",
-                    "UC8ggH3zU61XO0nMskSQwZdA",
-                ]
-            }
+        channel_list = [
+            "UCFqV9b9ji8yIMw3GyNlueuA",
+            "UC8ggH3zU61XO0nMskSQwZdA",
+        ]
+
+        rss_list = _scrap_rss_list(channel_list, parallel=True)
+        assert isinstance(rss_list, list)
+        assert rss_list
+        assert isinstance(rss_list[0], dict)
+
+        pd.DataFrame(rss_list).to_csv("./tmp/test_rss_list.csv", index=False)
+
+    def test_update_rss_list_details(self):
+        """ """
+
+        channel_list = [
+            "UCFqV9b9ji8yIMw3GyNlueuA",
+            "UC8ggH3zU61XO0nMskSQwZdA",
+        ]
+
+        rss_list = _scrap_rss_list(channel_list, parallel=True)
+
+        updated_rss_list_details = _update_rss_list(
+            rss_list, video_detail=True, categ_1=False, parallel=True
         )
 
-        pandarallel.initialize(nb_workers=1)
+        assert isinstance(updated_rss_list_details, list)
+        assert updated_rss_list_details
+        assert isinstance(updated_rss_list_details[0], dict)
 
-        df = build_rss(
-            df,
-            video_detail=True,
-            categ_1=True,
-            parrallel=True,
+        pd.DataFrame(updated_rss_list_details).to_csv(
+            "./tmp/updated_rss_list_details.csv", index=False
         )
 
-        cols = ["title", "id_categ_1"]
-        df = df.loc[:, cols]
-        # df.to_csv("tmp.csv", index=False)
+    def test_update_rss_list_categ1(self):
+        """ """
+
+        channel_list = [
+            "UCFqV9b9ji8yIMw3GyNlueuA",
+            "UC8ggH3zU61XO0nMskSQwZdA",
+        ]
+
+        rss_list = _scrap_rss_list(channel_list, parallel=True)
+
+        updated_rss_list_categ1 = _update_rss_list(
+            rss_list, video_detail=False, categ_1=True, parallel=True
+        )
+
+        assert isinstance(updated_rss_list_categ1, list)
+        assert updated_rss_list_categ1
+        assert isinstance(updated_rss_list_categ1[0], dict)
+
+        pd.DataFrame(updated_rss_list_categ1).to_csv(
+            "./tmp/updated_rss_list_categ1.csv", index=False
+        )
+
+    def test_update_rss_list_both(self):
+        """ """
+
+        channel_list = [
+            "UCFqV9b9ji8yIMw3GyNlueuA",
+            "UC8ggH3zU61XO0nMskSQwZdA",
+        ]
+
+        rss_list = _scrap_rss_list(channel_list, parallel=True)
+
+        updated_rss_list_both = _update_rss_list(
+            rss_list, video_detail=True, categ_1=True, parallel=True
+        )
+
+        assert isinstance(updated_rss_list_both, list)
+        assert updated_rss_list_both
+        assert isinstance(updated_rss_list_both[0], dict)
+
+        pd.DataFrame(updated_rss_list_both).to_csv(
+            "./tmp/updated_rss_list_both.csv", index=False
+        )
