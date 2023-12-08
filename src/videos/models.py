0a413218a8f1768bcb05aaab39ff2e0d8e6ad65e
AlexandreGazagnes <a_syoez@yahoo.fr>
2023-12-08 14:10:20 +0100

    add description

--- src/videos/models.py
+++ src/videos/models.py
@@ -13,6 +13,12 @@ DEFAULT_THUMBNAIL_VIDEO_URL = "https://i.ytimg.com/vi/kJQP7kiw5Fk/hqdefault.jpg?
 DEFAULT_DURATION = 360
 
 
+LOREM = """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur 
+sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."""
+
+
 class Video(Base):
     """ """
 
@@ -24,6 +30,7 @@ class Video(Base):
         nullable=False,
         unique=True,
     )
+
     title: Mapped[str] = mapped_column(
         String(150),
         nullable=False,
@@ -36,12 +43,14 @@ class Video(Base):
         nullable=False,
         unique=False,
     )
+
     keywords: Mapped[str] = mapped_column(
         String(120),
         default="None",
         nullable=False,
         unique=False,
     )
+
     thumbnail_video_url: Mapped[str] = mapped_column(
         String(300),
         default=DEFAULT_THUMBNAIL_VIDEO_URL,
@@ -62,30 +71,35 @@ class Video(Base):
         nullable=False,
         unique=False,
     )
+
     updated_at: Mapped[str] = mapped_column(
         DateTime(),
         default=make_now(),
         nullable=False,
         unique=False,
     )
+
     stars: Mapped[float] = mapped_column(
         Float(),
         default=-1.0,
         nullable=False,
         unique=False,
     )
+
     duration: Mapped[int] = mapped_column(
         Integer(),
         default=DEFAULT_DURATION,
         nullable=False,
         unique=False,
     )
+
     votes: Mapped[int] = mapped_column(
         Integer(),
         default=-1,
         nullable=False,
         unique=False,
     )
+
     views: Mapped[int] = mapped_column(
         Integer(),
         default=-1,
@@ -100,12 +114,19 @@ class Video(Base):
         # FK
     )
 
-    id_categ_1: Mapped[str] = mapped_column(
+    id_categ_0: Mapped[str] = mapped_column(
         String(12),
         default="?",
         unique=False,
         nullable=False,
         # FK
+    )
+
+    video_description: Mapped[str] = mapped_column(
+        String(500),
+        default=LOREM,
+        nullable=False,
+        unique=False,
     )
 
     def __repr__(self) -> str:
