08ddb5c6c09d633ac01ae1c716a597200394064a
AlexandreGazagnes <a_syoez@yahoo.fr>
2023-12-08 17:42:19 +0100

    upatde router

--- src/videos/routers.py
+++ src/videos/routers.py
@@ -56,93 +56,149 @@ async def delete_a_video(id_video: str):
 @videos.get("")
 async def get_all_videos(
     query: str | None = None,
+    skip: int = 0,
     limit: int = 200,
-    last_days: int = 7,
+    days_min: int = 0,
+    days_max: int = 30,
     duration_min: int = 3 * 60,
     duration_max: int = 10 * 3600,
-    # id_user: int | None = None,
     id_language: str | None = None,
     watched: int = -1,
     order_by: str = "published",
+    order_direction: str = "desc",
+    id_categ_0: str | None = None,
     id_categ_1: str | None = None,
-    # id_categ_2: list | None = None,
-    # id_status: list | None = None,
+    id_categ_2: str | None = None,
+    id_status: str | None = None,
 ):
     """Get all videos"""
 
-    # if query or id_categ_1 overide params
-    if query or id_categ_1:
-        last_days = 1_000
-        limit = 1_000
-
-    if query:
-        id_categ_1 = None
-        id_categ_2 = None
-        id_language = None
-        id_status = None
-        watched = -1
-
-    payload = VideoQuery.all(
-        query=query,
-        limit=limit,
-        last_days=last_days,
-        duration_min=duration_min,
-        duration_max=duration_max,
-        id_language=id_language,
-        id_categ_1=id_categ_1,
-        # id_categ_2=id_categ_2,
-        # id_status=id_status,
-        watched=watched,
-        order_by=order_by,
-        # id_user=id_user,
+    video_list, total = VideoQuery.all(
+        query,
+        skip,
+        limit,
+        days_min,
+        days_max,
+        duration_min,
+        duration_max,
+        id_language,
+        watched,
+        order_by,
+        order_direction,
+        id_categ_0,
+        id_categ_1,
+        id_categ_2,
+        id_status,
     )
-    return jsonify(payload)
+
+    return {
+        "videos": video_list,
+        "total": total,
+        "skip": skip,
+        "limit": limit,
+        "message": "done",
+        "params": {},
+    }
 
 
 @videos.get("/by_user", status_code=200)
 async def get_videos_by_user(
-    id_user: int | None = None,
+    id_user: int | None,
     query: str | None = None,
+    skip: int = 0,
     limit: int = 200,
-    last_days: int = 7,
+    days_min: int = 0,
+    days_max: int = 30,
     duration_min: int = 3 * 60,
     duration_max: int = 10 * 3600,
     id_language: str | None = None,
     watched: int = -1,
     order_by: str = "published",
+    order_direction: str = "desc",
+    id_categ_0: str | None = None,
     id_categ_1: str | None = None,
-    # id_categ_2: list | None = None,
-    # id_status: list | None = None,
+    id_categ_2: str | None = None,
+    id_status: str | None = None,
 ):
     """Get all videos"""
 
-    # if query overide params
-    if query or id_categ_1:
-        last_days = 1_000
-        limit = 1_000
-
-    if query:
-        id_categ_1 = None
-        id_categ_2 = None
-        id_language = None
-        id_status = None
-        watched = -1
-
-    payload = VideoQuery.by_user(
-        id_user=id_user,
-        query=query,
-        limit=limit,
-        last_days=last_days,
-        duration_min=duration_min,
-        duration_max=duration_max,
-        id_categ_1=id_categ_1,
-        # id_categ_2=id_categ_2,
-        id_language=id_language,
-        # id_status=id_status,
-        watched=watched,
-        order_by=order_by,
+    video_list, total = VideoQuery.by_user(
+        id_user,
+        query,
+        skip,
+        limit,
+        days_min,
+        days_max,
+        duration_min,
+        duration_max,
+        id_language,
+        watched,
+        order_by,
+        order_direction,
+        id_categ_0,
+        id_categ_1,
+        id_categ_2,
+        id_status,
     )
-    return jsonify(payload)
+
+    return {
+        "videos": video_list,
+        "total": total,
+        "skip": skip,
+        "limit": limit,
+        "message": "done",
+        "params": {},
+    }
+
+
+@videos.get("/by_channel", status_code=200)
+async def get_videos_by_channel(
+    id_channel: int | None,
+    query: str | None = None,
+    skip: int = 0,
+    limit: int = 200,
+    days_min: int = 0,
+    days_max: int = 30,
+    duration_min: int = 3 * 60,
+    duration_max: int = 10 * 3600,
+    id_language: str | None = None,
+    watched: int = -1,
+    order_by: str = "published",
+    order_direction: str = "desc",
+    id_categ_0: str | None = None,
+    id_categ_1: str | None = None,
+    id_categ_2: str | None = None,
+    id_status: str | None = None,
+):
+    """Get all videos"""
+
+    video_list, total = VideoQuery.by_channel(
+        id_channel,
+        query,
+        skip,
+        limit,
+        days_min,
+        days_max,
+        duration_min,
+        duration_max,
+        id_language,
+        watched,
+        order_by,
+        order_direction,
+        id_categ_0,
+        id_categ_1,
+        id_categ_2,
+        id_status,
+    )
+
+    return {
+        "videos": video_list,
+        "total": total,
+        "skip": skip,
+        "limit": limit,
+        "message": "done",
+        "params": {},
+    }
 
 
 # @videos.get("/by_categ_1")
@@ -191,15 +247,5 @@ async def get_videos_by_user(
 #     last_days: int = 2,
 # ):
 #     """Get all videos by watched"""
-
-#     pass
-
-
-# @videos.get("/by_channel")
-# async def get_videos_by_channel(
-#     limit: int = 100,
-#     last_days: int = 2,
-# ):
-#     """Get all videos by channel"""
 
 #     pass
