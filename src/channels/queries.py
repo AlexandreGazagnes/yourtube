0e5c55256e95ffa061366235150a372d39b36459
AlexandreGazagnes <a_syoez@yahoo.fr>
2023-12-11 12:06:02 +0100

    add tests

--- src/channels/queries.py
+++ src/channels/queries.py
@@ -3,24 +3,41 @@ from src.channels.models import Channel
 from src.db import Session, engine
 
 
-def _query_all():
+def _query_all_channels():
     """Return all channels"""
 
     with Session(engine) as session:
         results = session.query(Channel).all()
     results = [channel.dict() for channel in results]
-    return results
+
+    return results, len(results)
 
 
-def _query_all_id_channel():
+def _query_channel_by_id(id_channel: str):
+    """Return channel by id_channel"""
+
+    with Session(engine) as session:
+        result = session.query(Channel).filter_by(id_channel=id_channel).first()
+
+    result = result.dict()
+
+    return result
+
+
+def _query_channel_all_id():
     """Return all id_channel"""
 
     with Session(engine) as session:
         results = session.query(Channel.id_channel).all()
+
     results = [result[0] for result in results]
+
     return list(set(results))
 
 
 class ChannelQuery:
-    all = _query_all
-    all_id_channel = _query_all_id_channel
+    """ """
+
+    all = _query_channel_all_id
+    all_id = _query_channel_all_id
+    by_id_channel = _query_channel_by_id
---
+++ tests/unit/channels/test_channel_queries.py
@@ -0,0 +1,26 @@
+import logging
+
+# Import TestClient and your FastAPI app
+# from fastapi.testclient import TestClient
+# from src import app
+import pytest
+
+
+from src.channels.queries import ChannelQuery
+from src.channels.queries import _query_channel_by_id, _query_channels_by_user
+
+
+class TestChannelQueries:
+    """TestChannelQueries"""
+
+    @pytest.mark.parametrize("id_channel", [("UC-06IzcwOHu9_Xgqr-NMkUQ",)])
+    def test_channel_by_id(self, id_channel):
+        """test simple query"""
+
+        # id_channel = "UC-06IzcwOHu9_Xgqr-NMkUQ"  # yann leaonarid
+
+        results = _query_channel_by_id(id_channel)
+
+        assert isinstance(results, dict)
+
+        logging.info(results)
