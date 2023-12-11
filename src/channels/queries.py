4e9c9f9bd45c5e2c4cdcdee020d17d082b2b62d5
AlexandreGazagnes <a_syoez@yahoo.fr>
2023-12-11 15:10:58 +0100

    add results, total and new query

--- src/channels/queries.py
+++ src/channels/queries.py
@@ -32,20 +32,41 @@ def _query_channel_by_id(id_channel: str):
 ##################
 
 
-def _query_all_channels():
+def _query_all_ids_channels():
+    """Return all id_channel"""
+
+    with Session(engine) as session:
+        results = session.query(Channel.id_channel).all()
+
+    results = [result[0] for result in results]
+
+    # CAREFULL : PREVIOUS CODE USE JUST RETURN OF LIST NOT A TUPLE (results, len(results))
+
+    return list(set(results)), len(results)
+
+
+def _query_all_channels(
+    limit: int = 10_000,
+    skip: int = 0,
+    order_by: str | None = None,
+    order_direction: str = "desc",
+):
     """Return all channels"""
 
     with Session(engine) as session:
         results = session.query(Channel).all()
-    results = [channel.dict() for channel in results]
+    results = [channel.to_dict() for channel in results]
 
-    return results, len(results)
+    total = len(results)
+    results = results[skip : skip + limit]
+
+    return results, total
 
 
 def _query_channels_by_user(
     id_user: int,
-    limit: int | None = None,
-    skip: int | None = None,
+    limit: int | None = 1_000,
+    skip: int | None = 0,
     order_by: str = "id_channel",
     order_direction: str = "desc",
 ):
@@ -59,7 +80,6 @@ def _query_channels_by_user(
         LEFT JOIN channels c ON c.id_channel = uc.id_channel
         WHERE uc.id_user = {id_user}
         ORDER BY {order_by} {order_direction}
-        {"LIMIT " + str(limit) if limit else ""}
         ;
         """
 
@@ -67,20 +87,58 @@ def _query_channels_by_user(
 
     resuts = _perform_raw_query(query_string)
 
+    # to be investigated
+    # resuts = [result.to_dict() for result in resuts]
+
+    total = len(resuts)
+    resuts = resuts[skip : skip + limit]
+
     logging.info(resuts)
 
-    return resuts
+    return resuts, total
 
 
-def _query_all_ids_channels():
-    """Return all id_channel"""
+def _query_channels_by_categ_1(
+    id_user: int,
+    id_categ_1: str,
+    limit=1_000,
+    skip: int = 0,
+    order_by=None,
+    order_direction: str | None = None,
+) -> dict[str : list[dict]]:
+    query_string = f"""select c.id_channel, c.name, c.author, 
+    c.channel_description, c.created_at, c.updated_at, c.id_language, 
+    c.id_categ_1, c.thumbnail_channel_url
+    from userschannels uc
+    left join channels c on c.id_channel = uc.id_channel
+    where uc.id_user = {id_user}
+    ;
+    """
 
-    with Session(engine) as session:
-        results = session.query(Channel.id_channel).all()
+    logging.info(query_string)
 
-    results = [result[0] for result in results]
+    # gloabl query
+    resuts = _perform_raw_query(query_string)
 
-    return list(set(results))
+    # to be investigated
+    # resuts = [result.to_dict() for result in resuts]
+
+    # update
+    total = len(resuts)
+    resuts = resuts[skip : skip + limit]
+
+    # list unique id_categ_1 of results
+    categ_1_list = [result["id_categ_1"] for result in resuts]
+    categ_1_list = list(set(categ_1_list))
+
+    # resttuct data in dict of list
+    categ_1_dict = {categ_1: [] for categ_1 in categ_1_list}
+    for result in resuts:
+        categ_1_dict[result["id_categ_1"]].append(result)
+
+    logging.info(categ_1_dict)
+
+    return categ_1_dict, total
 
 
 class ChannelQuery:
@@ -96,3 +154,4 @@ class ChannelsQueries:
     all = _query_all_channels
     all_ids = _query_all_ids_channels
     by_user = _query_channels_by_user
+    by_categ_1 = _query_channels_by_categ_1
